023c801730f610a285818aa7d344ba40a7fe9b76
Daniel Uber <djuber@gmail.com>
2013-04-23 09:07:39 -0500

    added some accessors

--- sudoku.py
+++ sudoku.py
@@ -1,2 +1,26 @@
 class Sudoku():
-    pass
+    def __init__(self):
+        self._rows = self.empty_grid()
+    def empty_grid(self):
+        return [[0] * 9 ] * 9
+    def cols(self):
+        cols = self.empty_grid()
+        for col in range(9):
+            for row in range(9):
+                cols[col][row] = self.row(row)[col]
+        return cols
+    def col(self, n):
+        if 0 <= n and n < 9:
+            return self.cols()[n]
+        else:
+            raise IndexError
+    def __getitem__(self, index):
+        return self._rows[index]
+    def rows(self):
+        return self._rows
+    def row(self, n):
+        if 0 <= n and n < 9:
+            return self._rows[n]
+        else:
+            raise IndexError
+            
--- sudoku_tests.py
+++ sudoku_tests.py
@@ -2,12 +2,18 @@ import unittest
 from sudoku import *
 
 class SudokuTests(unittest.TestCase):
+    def setUp(self):
+        self.puzzle = Sudoku()
     def test_class_exists(self):
         "this test verifies a class Sudoku exists"
-        self.puzzle = Sudoku()
         self.assertTrue(self.puzzle)
-    
-
+    def test_initialized_to_zero(self):
+        self.assertEqual(0, self.puzzle.row(0)[0])
+        self.assertEqual(0, self.puzzle.row(8)[8])
+    def test_cols_gives_an_answer(self):
+        self.assertEqual(9, len(self.puzzle.cols()))
+    def test_col(self):
+        self.assertEqual(0, self.puzzle.col(4)[0])
 
 if __name__ == '__main__':
     unittest.main()
